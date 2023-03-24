041887cff544b0c64511ce43720865212132aa33
Antiba-Clarine <124131340+Antibaclarine@users.noreply.github.com>
2023-03-24 13:37:04 +0300

    Add files via upload

---
+++ distance-sensor-code.ino
@@ -0,0 +1,48 @@
+// C++ code
+//
+int distance = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  distance = 0.01723 * readUltrasonicDistance(6, 5);
+
+  Serial.println(distance);
+  if (distance <= 50) {
+    digitalWrite(7, HIGH);
+  } else {
+    digitalWrite(7, LOW);
+  }
+  if (distance <= 75) {
+    digitalWrite(8, HIGH);
+  } else {
+    digitalWrite(8, LOW);
+  }
+  if (distance <= 100) {
+    digitalWrite(9, HIGH);
+  } else {
+    digitalWrite(9, LOW);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
